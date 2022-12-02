ec12aa131bffb4d557e48c83f2993c8f5a9969c9
Nipinium <nipinium@gmail.com>
2022-12-02 14:05:12 +0700

    shorted input reading

--- day02/solve.cr
+++ day02/solve.cr
@@ -1,18 +1,11 @@
-def read_input(file : String)
-  a_map = {"A", "B", "C"}
-  b_map = {"X", "Y", "Z"}
+M = {"A" => 0, "B" => 1, "C" => 2, "X" => 0, "Y" => 1, "Z" => 2}
 
-  File.read_lines(file).reject!(&.empty?).map do |line|
-    a, b = line.split(" ")
-    {a_map.index!(a), b_map.index!(b)}
-  end
-end
-
-# input = read_input("day02/test0.txt")
-input = read_input("day02/input.txt")
+# input = File.read("day02/test0.txt").strip
+input = File.read("day02/input.txt").strip
+array = input.split(/\W+/).map { |x| M[x] }.each_slice(2).to_a
 
 # part 1
-puts input.sum { |a, b| {3, 0, 6}[(a - b) % 3] + b + 1 }
+puts array.sum { |(a, b)| {3, 0, 6}[(a - b) % 3] + b + 1 }
 
 # part 2
-puts input.sum { |a, b| {0, 3, 6}[b] + (a + {-1, 0, 1}[b]) % 3 + 1 }
+puts array.sum { |(a, b)| {0, 3, 6}[b] + (a + {-1, 0, 1}[b]) % 3 + 1 }
