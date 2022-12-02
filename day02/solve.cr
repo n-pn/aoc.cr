45b38b3ba8088d64bb3848603ab8013fba8fc585
Nipinium <nipinium@gmail.com>
2022-12-02 14:36:33 +0700

    futher shorten the solution

--- day02/solve.cr
+++ day02/solve.cr
@@ -1,11 +1,7 @@
-M = {"A" => 0, "B" => 1, "C" => 2, "X" => 0, "Y" => 1, "Z" => 2}
-
 # input = File.read("day02/test0.txt").strip
 input = File.read("day02/input.txt").strip
-array = input.split(/\W+/).map { |x| M[x] }.each_slice(2).to_a
 
-# part 1
-puts array.sum { |(a, b)| {3, 0, 6}[(a - b) % 3] + b + 1 }
-
-# part 2
-puts array.sum { |(a, b)| {0, 3, 6}[b] + (a + {-1, 0, 1}[b]) % 3 + 1 }
+puts input.each_line.reduce({0, 0}) { |(p1, p2), line|
+  a, b = line[0] - 'A', line[2] - 'X'
+  {p1 + (b - a + 1) % 3 * 3 + b + 1, p2 + b * 3 + (b + a - 1) % 3 + 1}
+}
