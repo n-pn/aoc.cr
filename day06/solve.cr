5828334a491da042ce1092134211b55d5b5e50bd
Nipinium <nipinium@gmail.com>
2022-12-06 16:28:02 +0700

    use proc

--- day06/solve.cr
+++ day06/solve.cr
@@ -1,5 +1,4 @@
 # tests = File.read("day06/test0.txt").lines
 input = File.read("day06/input.txt")
-
-puts input.each_char.cons(4).index!(&.uniq.size.== 4) + 4
-puts input.each_char.cons(14).index!(&.uniq.size.== 14) + 14
+solve = ->(x : Int32) { input.each_char.cons(x).index!(&.uniq.size.== x) + x }
+puts solve.call(4), solve.call(14)
