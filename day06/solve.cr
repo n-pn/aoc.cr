e6a43feeabd4393f948913d29cfb67b768642182
Nipinium <nipinium@gmail.com>
2022-12-06 16:20:26 +0700

    shorten day06

--- day06/solve.cr
+++ day06/solve.cr
@@ -1,5 +1,5 @@
-# tests = File.read("day06/test0.txt").strip.lines
-input = File.read("day06/input.txt").strip
+# tests = File.read("day06/test0.txt").lines
+input = File.read("day06/input.txt")
 
-puts input.chars.each_cons(4).with_index { |a, i| break i + 4 if a.uniq.size == 4 }
-puts input.chars.each_cons(14).with_index { |a, i| break i + 14 if a.uniq.size == 14 }
+puts input.each_char.cons(4).index!(&.uniq.size.== 4) + 4
+puts input.each_char.cons(14).index!(&.uniq.size.== 14) + 14
