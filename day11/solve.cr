4cc691f3578aa525ed97d0be6fd70b4edba93c01
Nipinium <nipinium@gmail.com>
2022-12-11 17:06:27 +0700

    cleanup

--- day11/solve.cr
+++ day11/solve.cr
@@ -7,23 +7,16 @@ class Monkey
   def initialize(input : String)
     input = input.sub("* old", "^ 2")
     @op_chr = input.match(/old (.)/).not_nil![1][0]
-
     _, *items, @op_val, @test, @if_true, @if_false = input.scan(/\d+/).map(&.[0].to_i)
-
     @p1 = items.map(&.to_i64)
     @p2 = @p1.dup
   end
 
-  def new_level(level : Int64)
-    case @op_chr
-    when '+' then level + @op_val
-    when '*' then level * @op_val
-    else          level * level
-    end
-  end
-
-  def pass_to(level : Int64)
-    level % @test == 0 ? @if_true : @if_false
+  def move(lvl : Int64, mod : Int32, div = 3)
+    val = @op_chr == '^' ? lvl : @op_val
+    lvl = @op_chr == '+' ? lvl + val : lvl * val
+    lvl = lvl % mod // div
+    {lvl, lvl % @test == 0 ? @if_true : @if_false}
   end
 end
 
@@ -31,25 +24,21 @@ end
 input = File.read("day11/input.txt").strip
 
 monkeys = input.split("\n\n").map { |x| Monkey.new(x) }
-factor = monkeys.product(&.test)
+mod = monkeys.product(&.test)
 
 20.times do
   monkeys.each do |m|
-    while level = m.p1.shift?
-      m.c1 += 1
-      level = m.new_level(level) // 3
-      monkeys[m.pass_to(level)].p1 << level
-    end
+    m.c1 += m.p1.size
+    m.p1.each { |l| l, i = m.move(l, mod, 3); monkeys[i].p1 << l }
+    m.p1.clear
   end
 end
 
 10000.times do
   monkeys.each do |m|
-    while level = m.p2.shift?
-      m.c2 += 1
-      level = m.new_level(level % factor)
-      monkeys[m.pass_to(level)].p2 << level
-    end
+    m.c2 += m.p2.size
+    m.p2.each { |l| l, i = m.move(l, mod, 1); monkeys[i].p2 << l }
+    m.p2.clear
   end
 end
 
