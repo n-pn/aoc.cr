0902061293fa396e18473611b7e358510fb8719a
Nipinium <nipinium@gmail.com>
2022-12-17 23:07:26 +0700

    convert points to numbers

--- day17/solve.cr
+++ day17/solve.cr
@@ -1,4 +1,5 @@
-SHAPES = {
+# initial shape of each blocks
+PROTOS = {
   [{0, 2}, {0, 3}, {0, 4}, {0, 5}],
   [{0, 3}, {1, 2}, {1, 3}, {1, 4}, {2, 3}],
   [{0, 2}, {0, 3}, {0, 4}, {1, 4}, {2, 4}],
@@ -6,73 +7,67 @@ SHAPES = {
   [{0, 2}, {0, 3}, {1, 2}, {1, 3}],
 }
 
-def overlap?(screen, shape)
-  shape.any? { |x, y| y < 0 || y > 6 || screen[y].includes?(x) }
-end
+WIDTH = 8 # can be 7 but 8 is faster to compute
 
-def capture_screen(screen, height, depth = 30)
+# reduce shape to list of integer
+SHAPES = PROTOS.map &.map { |(x, y)| x * WIDTH + y }
+
+def top_rows(screen, height, rows = 30) # can be 25, but 30 to make sure
   String.build do |str|
-    height.downto(height - depth) do |x|
-      7.times { |y| str << (screen[y].includes?(x) ? '#' : '.') }
+    height.downto(height - rows) do |x|
+      7.times { |y| str << (screen.includes?(x * WIDTH + y) ? '#' : '.') }
     end
   end
-end
-
-def solve(jet_dirs, limit = 3000)
-  heights = [0]
-  memoize = Hash(Int32, Hash(String, Tuple(Int32, Int32))).new
-
-  screen = Array(Set(Int32)).new(7) { Set(Int32).new }
-  7.times { |i| screen[i] << -1 }
-
-  jet_idx = 0
-  height = 0
-  cycle_found = nil
-
-  3000.times do |index|
-    shape = SHAPES[index % SHAPES.size].map { |x, y| {x + height + 3, y} }
-
-    loop do
-      jet_dir = jet_dirs[jet_idx]
-      jet_idx = (jet_idx + 1) % jet_dirs.size
-
-      shape_2 = shape.map { |x, y| {x, y + jet_dir} }
-      shape = shape_2 unless overlap?(screen, shape_2)
-
-      shape_2 = shape.map { |x, y| {x - 1, y} }
-      break if overlap?(screen, shape_2)
-      shape = shape_2
-    end
-
-    shape.each { |(x, y)| screen[y] << x }
-    height = {height, shape.max_of(&.[0]) + 1}.max
-    heights << height
-
-    next if cycle_found
-
-    memo = memoize[jet_idx * 5 + index % 5] ||= {} of String => {Int32, Int32}
-    hash = capture_screen(screen, height - 1)
-
-    if prev = memo[hash]?
-      cycle_found = {prev[0], index - prev[0], height - prev[1]}
-    else
-      memo[hash] = {index, height}
-    end
-  end
-
-  {heights, cycle_found.not_nil!}
 end
 
 # input = ">>><<><>><<<>><>>><<<>>><<<><<<>><>><<>>".strip
 input = File.read("day17/input.txt").strip
 
-jet_dirs = input.chars.map { |c| c == '<' ? -1 : 1 }
-heights, cycle = solve(jet_dirs)
+pushes = input.chars.map { |c| c == '<' ? -1 : 1 }
+push_id = 0
 
-start_idx, cycle_length, cycle_height = cycle.not_nil!
-upper_idx = 1000000000000 - start_idx
+max_heights = [0]
+state_memos = Hash(String, Tuple(Int32, Int32)).new
 
-cycle_count = upper_idx // cycle_length
+screen = Set(Int32).new
+7.times { |i| screen << (i - WIDTH) }
 
-puts heights[2022]
-puts cycle_count * cycle_height + heights[upper_idx % cycle_length + start_idx]
+max_height = 0
+cycle_data = nil
+
+3000.times do |index|
+  shape = SHAPES[index % SHAPES.size].map(&.+ (max_height + 3) * WIDTH)
+
+  loop do
+    shape_2 = shape.map(&.+ pushes[push_id])
+    push_id = (push_id + 1) % pushes.size
+
+    shape = shape_2 unless shape_2.any? { |x| !(x % WIDTH).in?(0..6) || x.in?(screen) }
+
+    shape_2 = shape.map(&.- WIDTH)
+    break if shape_2.any?(&.in?(screen))
+
+    shape = shape_2
+  end
+
+  screen.concat(shape)
+  max_height = {max_height, shape.max_of(&.// WIDTH) + 1}.max
+  max_heights << max_height
+
+  next if cycle_data
+
+  hash = "#{push_id}:#{index % 5}:#{top_rows(screen, max_height - 1)}"
+
+  if prev = state_memos[hash]?
+    cycle_data = {prev[0], index - prev[0], max_height - prev[1]}
+  else
+    state_memos[hash] = {index, max_height}
+  end
+end
+
+start_idx, cycle_length, cycle_height = cycle_data.not_nil!
+remain_iter = 1000000000000 - start_idx
+cycle_count = remain_iter // cycle_length
+
+puts max_heights[2022]
+puts cycle_count * cycle_height + max_heights[remain_iter % cycle_length + start_idx]
