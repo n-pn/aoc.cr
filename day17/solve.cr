c837d01ec84c5c832d1d7ff37ff41c9e55be1c51
Nipinium <nipinium@gmail.com>
2022-12-18 02:43:04 +0700

    use cycle in part 1 too

--- day17/solve.cr
+++ day17/solve.cr
@@ -9,10 +9,10 @@ PROTOS = {
 
 WIDTH = 8 # can be 7 but 8 is faster to compute
 
-# reduce shape to list of integer
+# reduce shape to list of integers
 SHAPES = PROTOS.map &.map { |(x, y)| x * WIDTH + y }
 
-def top_rows(screen, height, rows = 30) # can be 25, but 30 to make sure
+def top_rows(screen, height, rows = 25) # can be lower
   String.build do |str|
     height.downto(height - rows) do |x|
       7.times { |y| str << (screen.includes?(x * WIDTH + y) ? '#' : '.') }
@@ -21,7 +21,7 @@ def top_rows(screen, height, rows = 30) # can be 25, but 30 to make sure
 end
 
 # input = ">>><<><>><<<>><>>><<<>>><<<><<<>><>><<>>".strip
-input = File.read("day17/input.txt").strip
+input = {{ read_file("day17/input.txt").strip }}
 
 pushes = input.chars.map(&.- '=') # { |c| c == '<' ? -1 : 1 }
 push_id = 0
@@ -35,7 +35,7 @@ screen = Set(Int32).new
 max_height = 0
 cycle_data = nil
 
-3000.times do |index|
+(0..).each do |index|
   shape = SHAPES[index % SHAPES.size].map(&.+ (max_height + 3) * WIDTH)
 
   loop do
@@ -54,20 +54,19 @@ cycle_data = nil
   max_height = {max_height, shape.max_of(&.// WIDTH) + 1}.max
   max_heights << max_height
 
-  next if cycle_data # no need to track if cycle already found
-
   hash = "#{push_id * 5 + index % 5}:#{top_rows(screen, max_height - 1)}"
 
   if prev = state_memos[hash]?
     cycle_data = {prev[0], index - prev[0], max_height - prev[1]}
-  else
-    state_memos[hash] = {index, max_height}
+    break
   end
+
+  state_memos[hash] = {index, max_height}
 end
 
-start_idx, cycle_length, cycle_height = cycle_data.not_nil!
-remain_iter = 1000000000000 - start_idx
-cycle_count = remain_iter // cycle_length
-
-puts max_heights[2022]
-puts cycle_count * cycle_height + max_heights[remain_iter % cycle_length + start_idx]
+{2022, 1000000000000}.each do |part|
+  start_idx, cycle_length, cycle_height = cycle_data.not_nil!
+  remain_iter = part - start_idx
+  cycle_count = remain_iter // cycle_length
+  puts cycle_count * cycle_height + max_heights[remain_iter % cycle_length + start_idx]
+end
