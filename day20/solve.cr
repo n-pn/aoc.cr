49f32037c6f6c0bfd7f307c603cc7636ede9fd0e
Nipinium <nipinium@gmail.com>
2022-12-20 16:47:57 +0700

    add solution to day 20 using inserting and deleting array

--- day20/solve.cr
+++ day20/solve.cr
@@ -1,5 +1,6 @@
 def solve(input, times)
-  nexts = Array.new(input.size) { |i| (i + 1) % input.size }
+  limit = input.size
+  nexts = Array.new(limit) { |i| (i + 1) % limit }
 
   times.times do
     input.each_with_index do |value, index|
@@ -8,24 +9,37 @@ def solve(input, times)
       left, right = index, nexts[index]
       nexts[nexts.index!(&.== index)] = right
 
-      (value % (input.size - 1)).times { left, right = right, nexts[right] }
+      (value % (limit - 1)).times { left, right = right, nexts[right] }
 
       nexts[left] = index
       nexts[index] = right
     end
   end
 
-  start = input.index!(&.== 0)
-
-  {1000, 2000, 3000}.sum do |idx|
-    right = start
-    (idx % input.size).times { right = nexts[right] }
-    input[right]
-  end
+  start = input.index!(0)
+  {1000, 2000, 3000}.sum { |i| input[0.to(i % limit - 1).reduce(start) { |r, _| nexts[r] }] }
 end
 
-input = {{ read_file("day20/input.txt").strip }}.lines.map(&.to_i64)
 # input = {{ read_file("day20/test0.txt").strip }}.lines.map(&.to_i64)
+input = {{ read_file("day20/input.txt").strip }}.lines.map(&.to_i64)
 
-puts solve(input, 1)
-puts solve(input.map(&.* 811589153), 10)
+def solve_2(input, times = 1)
+  indexes = (0...input.size).to_a
+
+  times.times do
+    input.each_with_index do |v, i|
+      j = indexes.index!(i)
+      indexes.delete_at(j)
+      indexes.insert((v + j) % indexes.size, i)
+    end
+  end
+
+  start = indexes.index!(input.index!(0))
+  {1000, 2000, 3000}.sum { |idx| input[indexes[(start + idx) % input.size]] }
+end
+
+# puts solve(input, 1)
+# puts solve(input.map(&.* 811589153), 10)
+
+puts solve_2(input, 1)
+puts solve_2(input.map(&.* 811589153), 10)
