e7b6657f825c5b8a322a8dac2bb7ba940aa6d3a1
Nipinium <nipinium@gmail.com>
2022-12-21 15:41:37 +0700

    cleanup

--- 2022/21/solve.cr
+++ 2022/21/solve.cr
@@ -1,59 +1,62 @@
-input = {{ read_file("2022/21/test0.txt").strip }}
+def solve(rules, values, target = "root", part2 = false)
+  values.delete(target)
+  changed = true
+
+  while changed
+    changed = false
+
+    rules.each_with_index do |(monkey, lh, rh, op), i|
+      left, right = values[lh]?, values[rh]?
+
+      if part2 && monkey == "root"
+        next unless num = left || right
+        values[lh] = values[rh] = num
+        rules.delete_at(i)
+        changed = true
+      elsif left && right
+        values[monkey] = op.call(left, right)
+        rules.delete_at(i)
+        changed = true
+      end
+    end
+  end
+
+  values[target]
+end
+
+# input = {{ read_file("2022/21/test0.txt").strip }}
 input = {{ read_file("2022/21/input.txt").strip }}
 
-DEPS = [] of {String, String, String, String}
-NUMS = {} of String => Int64
+rules = [] of {String, String, String, Proc(Int64, Int64, Int64)}
+values = {} of String => Int64
+
+def build(op : String)
+  case op
+  when "+" then ->(l : Int64, r : Int64) { l + r }
+  when "-" then ->(l : Int64, r : Int64) { l - r }
+  when "*" then ->(l : Int64, r : Int64) { l * r }
+  when "/" then ->(l : Int64, r : Int64) { l // r }
+  else          raise "Unsupport"
+  end
+end
 
 input.each_line do |line|
-  monkey, right = line.split(": ")
-  next if monkey == "humn"
+  left, right = line.split(": ")
 
-  if number = right.to_i64?
-    NUMS[monkey] = number
+  if value = right.to_i64?
+    values[left] = value
   else
-    left, operator, right = right.split(' ')
-    DEPS << {monkey, left, right, operator}
+    lhs, op, rhs = right.split(' ')
+    rules << {left, lhs, rhs, build(op)}
 
-    case operator
-    when "+" then DEPS << {left, monkey, right, "-"} << {right, monkey, left, "-"}
-    when "-" then DEPS << {left, monkey, right, "+"} << {right, left, monkey, "-"}
-    when "*" then DEPS << {left, monkey, right, "/"} << {right, monkey, left, "/"}
-    when "/" then DEPS << {left, monkey, right, "*"} << {right, left, monkey, "/"}
+    case op
+    when "+" then rules << {lhs, left, rhs, build("-")} << {rhs, left, lhs, build("-")}
+    when "-" then rules << {lhs, left, rhs, build("+")} << {rhs, lhs, left, build("-")}
+    when "*" then rules << {lhs, left, rhs, build("/")} << {rhs, left, lhs, build("/")}
+    when "/" then rules << {lhs, left, rhs, build("*")} << {rhs, lhs, left, build("/")}
     end
   end
 end
 
-NUMS.delete("humn")
-
-loop do
-  changed = false
-
-  DEPS.each_with_index do |(monkey, lh, rh, op), i|
-    if monkey == "root"
-      if num = NUMS[lh]? || NUMS[rh]?
-        NUMS[lh] = NUMS[rh] = num
-        changed = true
-        DEPS.delete_at(i)
-      end
-
-      next
-    end
-
-    next unless (left = NUMS[lh]?) && (right = NUMS[rh]?)
-    DEPS.delete_at(i)
-
-    changed = true
-    NUMS[monkey] =
-      case op
-      when "+" then left + right
-      when "-" then left - right
-      when "*" then left * right
-      when "/" then left // right
-      else          0_i64
-      end
-  end
-
-  break unless changed
-end
-
-puts NUMS["humn"]?
+puts solve(rules.dup, values.dup, "root")
+puts solve(rules, values, "humn", true)
