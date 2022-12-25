007d323eab6f9267d8535f31a663583decd7918c
Nipinium <nipinium@gmail.com>
2022-12-25 13:39:45 +0700

    cleanup

--- 2022/25/solve.cr
+++ 2022/25/solve.cr
@@ -4,14 +4,13 @@ input = {{ read_file("2022/25/input.txt").strip }}
 MAP = {'0' => 0, '1' => 1, '2' => 2, '-' => -1, '=' => -2}
 
 sum = input.lines.sum { |l| l.chars.reduce(0_i64) { |a, c| a * 5 + MAP[c] } }
-res = String::Builder.new
+res = [] of Char
 
 while sum != 0
-  r = sum % 5
-  chr, num = MAP.key_for?(r).try { |c| {c, 0} } || MAP.find! { |_, n| MAP.has_value?(r + n) }
-
-  sum = (sum - num) // 5
-  res << chr
+  rem = sum % 5
+  rem -= 5 if rem > 2
+  sum = (sum - rem) // 5
+  res.unshift MAP.key_for(rem)
 end
 
-puts res.to_s.reverse
+puts res.join
