034175ba2722536dacabb5407ccff6fe0e070768
NPN <me@npn.am>
2023-12-04 15:00:29 +0700

    remove input intermediate array

--- 2023/04/shorter.cr
+++ 2023/04/shorter.cr
@@ -1,12 +1,12 @@
 require "bit_array"
 time = Time.measure do
-  text = File.read_lines("#{__DIR__}/bigboy.txt")
+  data = Array(Int32).new(initial_capacity: 200000)
 
-  data = text.map do |line|
+  File.each_line("#{__DIR__}/bigboy.txt") do |line|
     win, you = line.split(':', 2).last.split('|', 2)
     set = BitArray.new(100)
     win.split { |i| set[i.to_i] = true }
-    you.split.count { |i| set[i.to_i] }
+    data << you.split.count { |i| set[i.to_i] }
   end
 
   puts data.sum { |x| 1 << x - 1 }
