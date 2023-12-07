4f0e6ebcad853c8169f3a8f903b91c4be620e98d
NPN <me@npn.am>
2023-12-07 13:58:27 +0700

    shorten solution

--- 2023/07/solve.cr
+++ 2023/07/solve.cr
@@ -9,91 +9,36 @@ QQQJA 483
 TXT
 
 struct Hand
-  def initialize(cards : String)
-    @cards = cards.chars
+  getter bid = 0, value = 0, cards : Array(Char)
+
+  def initialize(line : String, ranking, use_joker = false)
+    cards, bid = line.split
+    @cards, @bid = cards.chars, bid.to_i
+
     @tally = @cards.tally
+    self.use_joker! if use_joker && @tally['J']? != 5
+
+    @cards.each { |c| @value = @value * ranking.size + ranking.index!(c) }
+    (2..5).each { |x| @value += @tally.values.count(x) * 1_000_000 * x * x }
   end
 
-  def has?(c)
-    @tally.any?(&.[1].== c)
-  end
-
-  def has32?
-    has?(3) && has?(2)
-  end
-
-  def has22?
-    @tally.count(&.[1].== 2) == 2
-  end
-
-  include Comparable(self)
-
-  class_property ranking = "AKQJT98765432".chars
-
-  def rank(i)
-    @@ranking.index!(@cards[i])
-  end
-
-  def <=>(other : self)
-    high = 5.times do |i|
-      h = other.rank(i) <=> rank(i)
-      break h unless h == 0
-    end
-
-    case
-    when self.has?(5)  then other.has?(5) ? high : 1
-    when other.has?(5) then -1
-    when self.has?(4)  then other.has?(4) ? high : 1
-    when other.has?(4) then -1
-    when self.has32?   then other.has32? ? high : 1
-    when other.has32?  then -1
-    when self.has?(3)  then other.has?(3) ? high : 1
-    when other.has?(3) then -1
-    when self.has22?   then other.has22? ? high : 1
-    when other.has22?  then -1
-    when self.has?(2)  then other.has?(2) ? high : 1
-    when other.has?(2) then -1
-    else                    high
-    end
-  end
-
-  def use_joker!
-    return unless t = @tally['J']?
-
-    4.downto(1) do |x|
-      @tally.each do |k, v|
-        next if k == 'J' || v != x
-
-        @tally[k] += t
-        @tally['J'] = 0
-
-        return
-      end
-    end
-  end
-
-  def inspect(io : IO) : Nil
-    @cards.join(io)
+  def use_joker! : Nil
+    k = @tally.to_a.select(&.[0].!= 'J').max_by(&.[1]).first
+    @tally[k] += @tally.delete('J') || 0
   end
 end
 
-def solve(text, use_joker = false)
-  data = text.map do |line|
-    cards, bid = line.split ' ', 2
-    hand = Hand.new(cards)
-    hand.use_joker! if use_joker
-    {hand, bid.to_i}
-  end
-
-  data.sort!.map_with_index(1) { |(_, bid), i| bid * i }.sum
+def solve(text, ranking, use_joker = false)
+  data = text.map { |line| Hand.new(line, ranking, use_joker) }
+  data.sort_by!(&.value).map_with_index(1) { |c, i| c.bid * i }.sum
 end
 
-if solve(test1) == 6440
-  puts solve(input)
+ranking = "AKQJT98765432".chars.reverse!
+if solve(test1, ranking) == 6440
+  puts solve(input, ranking)
 end
 
-Hand.ranking = "AKQT98765432J".chars
-
-if solve(test1, true) == 5905
-  puts solve(input, true)
+ranking = "AKQT98765432J".chars.reverse!
+if solve(test1, ranking, true) == 5905
+  puts solve(input, ranking, true)
 end
