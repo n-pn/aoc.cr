1012dda433c120a4d212e3c22870992c771fa6b0
NPN <me@npn.am>
2023-12-12 18:44:34 +0700

    cleanup

--- 2023/12/solve.cr
+++ 2023/12/solve.cr
@@ -11,32 +11,32 @@ TXT
 
 CACHE = Hash({String, Array(Int32)}, Int64).new
 
-def solve(text : String, ints : Array(Int32))
+def calc(text : String, ints : Array(Int32))
   CACHE.put_if_absent({text, ints}) do
     break 0_i64 if text.count(&.!= '.') < ints.sum
     break text.includes?('#') ? 0_i64 : 1_i64 if ints.empty?
 
     char = text[0]
-    left, *right = ints
+    head, *tail = ints
 
-    sum1 = char.in?('.', '?') ? solve(text[1..], ints) : 0_i64
-    break sum1 if char == '.' || !text[0..left].matches?(/^[?#]+[?.]$/)
+    sum1 = char.in?('.', '?') ? calc(text[1..], ints) : 0_i64
+    break sum1 if char == '.' || text[0..head] !~ (/^[?#]+[?.]$/)
 
-    sum1 + solve(text[(left + 1)..], right)
+    sum1 + calc(text[(head + 1)..], tail)
   end
 end
 
 def part1(text)
   text.lines.sum do |line|
     str, ints = line.split(' ', 2)
-    solve(str + '.', ints.split(',').map(&.to_i))
+    calc(str + '.', ints.split(',').map(&.to_i))
   end
 end
 
 def part2(text)
   text.lines.sum do |line|
     str, ints = line.split(' ', 2)
-    solve([str].*(5).join('?') + '.', ints.split(',').map(&.to_i) * 5)
+    calc([str].*(5).join('?') + '.', ints.split(',').map(&.to_i) * 5)
   end
 end
 
