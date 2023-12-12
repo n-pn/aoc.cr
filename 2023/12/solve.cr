aa3a7225a9eaef558b9f4f6f6db74a7fe4dec688
NPN <me@npn.am>
2023-12-12 18:18:08 +0700

    solve day 12

---
+++ 2023/12/solve.cr
@@ -0,0 +1,44 @@
+input = File.read("#{__DIR__}/input.txt").strip
+
+test1 = <<-TXT
+???.### 1,1,3
+.??..??...?##. 1,1,3
+?#?#?#?#?#?#?#? 1,3,1,6
+????.#...#... 4,1,1
+????.######..#####. 1,6,5
+?###???????? 3,2,1
+TXT
+
+CACHE = Hash({String, Array(Int32)}, Int64).new
+
+def solve(text : String, ints : Array(Int32))
+  CACHE.put_if_absent({text, ints}) do
+    break 0_i64 if text.count(&.!= '.') < ints.sum
+    break text.includes?('#') ? 0_i64 : 1_i64 if ints.empty?
+
+    char = text[0]
+    left, *right = ints
+
+    sum1 = char.in?('.', '?') ? solve(text[1..], ints) : 0_i64
+    break sum1 if char == '.' || !text[0..left].matches?(/^[?#]+[?.]$/)
+
+    sum1 + solve(text[(left + 1)..], right)
+  end
+end
+
+def part1(text)
+  text.lines.sum do |line|
+    str, ints = line.split(' ', 2)
+    solve(str + '.', ints.split(',').map(&.to_i))
+  end
+end
+
+def part2(text)
+  text.lines.sum do |line|
+    str, ints = line.split(' ', 2)
+    solve([str].*(5).join('?') + '.', ints.split(',').map(&.to_i) * 5)
+  end
+end
+
+puts "part 1: #{part1(input)}" if part1(test1) == 21
+puts "part 2: #{part2(input)}" if part2(test1) == 525152
