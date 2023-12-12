350b9a7553e24c60a19412637c3583164d135f9a
NPN <me@npn.am>
2023-12-12 21:15:24 +0700

    add time measurement for bigboy

--- 2023/12/solve.cr
+++ 2023/12/solve.cr
@@ -1,4 +1,5 @@
 input = File.read("#{__DIR__}/input.txt").strip
+# input = File.read("#{__DIR__}/bigboy.txt").strip
 
 test1 = <<-TXT
 ???.### 1,1,3
@@ -22,7 +23,7 @@ def calc(text : String, ints : Array(Int32))
     sum1 = char.in?('.', '?') ? calc(text[1..], ints) : 0_i64
     break sum1 if char == '.' || text[0..head] !~ (/^[?#]+[?.]$/)
 
-    sum1 + calc(text[(head + 1)..], tail)
+    sum1 &+ calc(text[(head + 1)..], tail)
   end
 end
 
@@ -40,5 +41,9 @@ def part2(text)
   end
 end
 
-puts "part 1: #{part1(input)}" if part1(test1) == 21
-puts "part 2: #{part2(input)}" if part2(test1) == 525152
+time = Time.measure do
+  puts "part 1: #{part1(input)}" # if part1(test1) == 21
+  puts "part 2: #{part2(input)}" # if part2(test1) == 525152
+end
+
+puts time.total_milliseconds
