bf887a126a9c074dc57cbb2a6e69a64a1e021f39
NPN <me@npn.am>
2024-12-03 14:22:20 +0700

    improve day 03

--- 2024/03/solve.cr
+++ 2024/03/solve.cr
@@ -1,6 +1,4 @@
-input = File.read("#{__DIR__}/input.txt").strip
+input = File.read("#{__DIR__}/input.txt").gsub('\n', "")
 
 solve = ->(str : String) { str.scan(/mul\((\d+),(\d+)\)/).sum { |x| x[1].to_i * x[2].to_i } }
-
-puts solve.call(input)
-puts "do()#{input}".split(/(do\(\)|don't\(\))/).each_cons_pair.sum { |a, b| a == "do()" ? solve.call(b) : 0 }
+puts solve.call(input), solve.call(input.gsub(/don't\(\).*?do\(\)/, "do()"))
