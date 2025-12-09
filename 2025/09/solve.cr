139680a6fb6083b5bff13cd40599ca531011c848
NPN <me@npn.am>
2025-12-09 23:01:47 +0700

    make it run faster

--- 2025/09/solve.cr
+++ 2025/09/solve.cr
@@ -24,26 +24,20 @@ def part2(input : Array(String))
 
   arr << arr[0]
   arr.each_cons_pair do |(ax, ay), (bx, by)|
-    ax = x_map[ax]
-    ay = y_map[ay]
-    bx = x_map[bx]
-    by = y_map[by]
-
     if ax == bx
-      ay.to(by).each { |i| inner << {ax, i} }
+      y_map[ay].to(y_map[by]).each { |i| inner << {x_map[ax], i} }
     else
-      ax.to(bx).each { |i| inner << {i, ay} }
+      x_map[ax].to(x_map[bx]).each { |i| inner << {i, y_map[ay]} }
     end
   end
 
   arr.pop
 
   x_min, y_min = arr.min_by { |a, b| {a, b} }
-
   start = {x_map[x_min] + 1, y_map[y_min] + 1}
   inner << start
-
   queue = [start]
+
   queue.each do |(x, y)|
     [{0, -1}, {0, 1}, {-1, 0}, {1, 0}].each do |i, j|
       next if inner.includes?({x + i, y + j})
@@ -53,9 +47,13 @@ def part2(input : Array(String))
   end
 
   max = 0
+
   arr.each_with_index do |(ax, ay), i|
     arr[i + 1..].each do |bx, by|
-      next unless x_map[ax].to(x_map[bx]).all? { |k| y_map[ay].to(y_map[by]).all? { |l| inner.includes?({k, l}) } }
+      next unless x_map[ax].to(x_map[bx]).all? { |x| inner.includes?({x, y_map[ay]}) }
+      next unless x_map[ax].to(x_map[bx]).all? { |x| inner.includes?({x, y_map[by]}) }
+      next unless y_map[ay].to(y_map[by]).all? { |y| inner.includes?({x_map[ax], y}) }
+      next unless y_map[ay].to(y_map[by]).all? { |y| inner.includes?({x_map[bx], y}) }
       max = {(ax - bx).abs.+(1) * (ay - by).abs.+(1), max}.max
     end
   end
